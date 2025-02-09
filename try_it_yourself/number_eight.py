314d6315d260beee9d14372737b6dec0f0d3e292
Chndu01 <contactchandrakantt@gmail.com>
2025-02-09 21:25:25 +0530

    new commit

---
+++ try_it_yourself/number_eight.py
@@ -0,0 +1,9 @@
+print(5 + 3)
+print(10-2)
+print(16/2)
+print(4*2)
+
+# My favourite Number 
+
+number = 4
+print("my favourite number is : ", number)
--- variables.py
+++ variables.py
@@ -1,59 +1,94 @@
-# string 
+# # string 
 
-new_string = "This is a string"
-new_string_1 = "This is also a string "
+# new_string = "This is a string"
+# new_string_1 = "This is also a string "
 
-message_string = 'I told my friend, "Python is my favourite Launguage!"'
-message_string_1 = "The launguage 'Python' is named after Monty Python, not the snake."
-message_string_2 = "one of python's strength is its diverse and supportive community."
-
-
-name = "hello chandu"
-print(name.title())
-print(name.upper())
-print(name.lower())
-
-first_name = "chandrakant"
-last_name = "netam"
-full_name = f"{first_name} {last_name}"
-print(full_name)
-
-first_name = "chandrakant"
-last_name = "netam"
-full_name = f"{first_name} {last_name}"
-print(f"Hello, {full_name.upper()}")
-
-first_name = "chandrakant"
-last_name = "netam"
-full_name = f"{first_name} {last_name}"
-message = f"Hello, {full_name.title()} You're Welcome."
-print(message)
-
-print("Launguage: \nPython \nC \nJavascript")
-print("\tPython")
-
-print("Launguage: \n\tPython \n\tC \n\tJavascript")
-
-favourite_language = "python "
-print(favourite_language)
-print(favourite_language.rstrip())
-
-favourite_language_1 = " Python"
-print(favourite_language_1)
-print(favourite_language_1.lstrip())
+# message_string = 'I told my friend, "Python is my favourite Launguage!"'
+# message_string_1 = "The launguage 'Python' is named after Monty Python, not the snake."
+# message_string_2 = "one of python's strength is its diverse and supportive community."
 
 
-favourite_language = " python"
-favourite_language = favourite_language.lstrip()
-print(favourite_language)
+# name = "hello chandu"
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
 
-favourite_language = " python "
-favourite_language = favourite_language.rstrip()
-favourite_language =favourite_language.lstrip()
-print(favourite_language)
+# first_name = "chandrakant"
+# last_name = "netam"
+# full_name = f"{first_name} {last_name}"
+# print(full_name)
+
+# first_name = "chandrakant"
+# last_name = "netam"
+# full_name = f"{first_name} {last_name}"
+# print(f"Hello, {full_name.upper()}")
+
+# first_name = "chandrakant"
+# last_name = "netam"
+# full_name = f"{first_name} {last_name}"
+# message = f"Hello, {full_name.title()} You're Welcome."
+# print(message)
+
+# print("Launguage: \nPython \nC \nJavascript")
+# print("\tPython")
+
+# print("Launguage: \n\tPython \n\tC \n\tJavascript")
+
+# favourite_language = "python "
+# print(favourite_language)
+# print(favourite_language.rstrip())
+
+# favourite_language_1 = " Python"
+# print(favourite_language_1)
+# print(favourite_language_1.lstrip())
 
 
-nostarch_url = 'https://nostarch.com'
-simple_url = nostarch_url.removeprefix('https://')
-print(simple_url)
+# favourite_language = " python"
+# favourite_language = favourite_language.lstrip()
+# print(favourite_language)
+
+# favourite_language = " python "
+# favourite_language = favourite_language.rstrip()
+# favourite_language =favourite_language.lstrip()
+# print(favourite_language)
+
+
+# nostarch_url = 'https://nostarch.com'
+# simple_url = nostarch_url.removeprefix('https://')
+# print(simple_url)
+
+# Numbers 
+
+# Integers 
+print(2 + 3)
+print(3 - 2)
+print(2 * 3)
+print(3 / 2)
+print(3 ** 2)
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+
+# Floats
+
+print(2 + 2.0)
+print(0.1 * 2)
+print(4 / 2)
+
+# Multiple assignments 
+
+x , y , z = 10 , 20 , 30
+print(x)
+print(y)
+print(z)
+
+
+# Underscores in Numbers
+
+universe_age = 14_000_000_000
+print(universe_age)
+
+my_birth_date = 4_12_2003
+print(my_birth_date)
+
 
