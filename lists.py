f508b05884e5061dadfd1fc4b139932547dff84b
Chndu01 <contactchandrakantt@gmail.com>
2025-02-10 08:36:43 +0530

    New addition

---
+++ lists.py
@@ -0,0 +1,11 @@
+# Creating a list
+
+bicycle = [ 'trek','cannodale','redline','specialized']
+print(bicycle)
+
+#selecting indiviadual value list 
+print(bicycle[3])
+print(bicycle[2].upper())
+
+message = f"My first bicycle was a {bicycle[0].title()}"
+print(message)
---
+++ try_it_yourself/lists.py
@@ -0,0 +1,20 @@
+names = ['Chandu','Dharam','Ramesh','Kiran','Aayush']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+print(f"Hello {names[0]}")
+print(f"Hello {names[1]}")
+print(f"Hello {names[2]}")
+print(f"Hello {names[3]}")
+print(f"Hello {names[4]}") 
+
+# Think of your favourite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as "I would like to own a Honda Motorcycle."
+
+favourite = ['Honda City','BMW','Fortuner','Pulsur','RangeRover','Buggati']
+print(f"I want to drive a {favourite[0]} car.")
+print(f"I would like to own a {favourite[4]}.")
+print(f"I want to drive {favourite[1]} Motorcycle.")
+
