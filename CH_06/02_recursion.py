84a5861504572912d0762261513cfad2dd9e3d6a
Chndu01 <contactchandrakantt@gmail.com>
2025-08-21 14:55:58 +0530

    Recursion

--- CH_06/02_recursion.py
+++ CH_06/02_recursion.py
@@ -1,9 +1,20 @@
 # Recursion - When a function calls iteself repeatedly
+# Recursiove Function
 
 def show(n):
     if(n == 0):
         return
     print(n)
-    show(n-1)
+    show(n-1)  # calling function again
 
 show(10)
+
+
+
+def fact(n):
+    if(n == 0 or n == 1):
+        return 1
+    else:
+        return n * fact(n-1)
+    
+print(fact(5))
